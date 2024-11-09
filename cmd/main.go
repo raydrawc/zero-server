ce72cb827d64aea5626c93b49cb56beb82e09252
genet <raydraw@163.com>
2024-11-09 12:17:57 +0800

    feat: first push

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
+import "github.com/gin-gonic/gin"
+
+func init() {
+	// 加载配置
+
+	// 初始化基础库
+}
+
 func main() {
 	print("hello world")
+	HttpServer()
+}
+
+func HttpServer() {
+	// 启动http服务
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		c.String(200, "hello world")
+	})
+
+	r.Run(":8080")
 }
